cfd49905b44efc04bdc83af6b41b4f72fb5639c1
HeGaofeng <gaofeng.he@outlook.com>
2020-05-28 23:37:56 +0800

    Python Code

---
+++ feature_extract/__label__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import label_log
+import config_manager
+import os
+if __name__ == "__main__":
+    dataset_path = config_manager.read_config()
+    if dataset_path == -1:
+        raise ValueError
+    malicious_path = dataset_path + "\\Malicious"
+    malicious_folder_path = config_manager.get_folders_name(malicious_path)
+    for dir_name in malicious_folder_path:
+       # print ("xx-Debug:"+dir_name)
+         # modify by hegaofeng
+         path_to_single = malicious_path + "\\" + dir_name
+         if os.path.isdir(path_to_single): # add by hegaofeng
+             label_log.label_conn_log(path_to_single)
+
+    # We only aim to detect malicious TLS traffic, thus, we consider all tested traffic are malicious by default.
+    '''  
+    normal_path = dataset_path + "\\Normal"
+ 
+    normal_folder_path = config_manager.get_folders_name(normal_path)
+    # process normal dataset
+    for dir_name in normal_folder_path:
+       # print ("hgf:"+dir_name)
+        #if os.path.isdir(dir_name): # modify by hegaofeng
+        path_to_single = normal_path + "\\" + dir_name
+        if os.path.isdir(path_to_single): # add by hegaofeng
+            label_log.label_conn_log(path_to_single)
+    '''
